7c523312190572e141b7ca0c4d974fcf30df4af2
abersheeran <me@abersheeran.com>
2021-10-13 23:55:46 +0800

    Add more tests

--- tests/test_importlib.py
+++ tests/test_importlib.py
@@ -1,15 +1,17 @@
 import sys
 from pathlib import Path
 
+import pytest
+
 from mingshe.importlib import install_meta, uninstall_meta
 
 
 def test_importlib():
-    install_meta(".she")
-
     path = str(Path(__file__).absolute().parent / "for_test_importlib")
     sys.path.insert(0, path)
     try:
+        install_meta(".she")
+
         import a
         assert a.name == "a"
 
@@ -19,7 +21,14 @@ def test_importlib():
         from t.b import name
         assert name == "t.b"
 
+        uninstall_meta(".she")
+
+        del sys.modules["a"]
+        del sys.modules["p"]
+        del sys.modules["t.b"]
+
+        with pytest.raises(ImportError):
+            import a
+
     finally:
         sys.path.remove(path)
-
-        uninstall_meta(".she")
